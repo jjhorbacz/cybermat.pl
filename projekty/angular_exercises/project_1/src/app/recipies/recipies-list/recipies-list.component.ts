6285c2a97477ff393abc42a04f76152ce1bbc9c4
Xedilius <j.j.horbacz@gmail.com>
2023-12-11 15:35:29 +0100

    working

--- projekty/angular_exercises/project_1/src/app/recipies/recipies-list/recipies-list.component.ts
+++ projekty/angular_exercises/project_1/src/app/recipies/recipies-list/recipies-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipie } from '../recipies.model';
 
 @Component({
   selector: 'app-recipies-list',
   templateUrl: './recipies-list.component.html',
   styleUrls: ['./recipies-list.component.scss']
 })
-export class RecipiesListComponent {
+export class RecipiesListComponent implements OnInit {
+
+  recipes: Recipie[] = [
+    new Recipie('testowa', 'to tylko test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
 
 }
